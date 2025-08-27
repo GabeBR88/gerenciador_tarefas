b6294e23da10dd4ea0425e4648da2043fe2bd3d2
Gabriel Brito <gabrielbrito.contato2017@gmail.com>
2025-08-27 11:47:08 -0300

    Criação da classe Tarefa

--- models/classe_tarefa.py
+++ models/classe_tarefa.py
@@ -1,3 +1,12 @@
 class Tarefa:
     def __init__(self, titulo:str, descricao:str, data:str, status:str, id_:int=None):
-        pass
+        self.id = id_
+        self.titulo = titulo
+        self.descricao = descricao
+        self.data = data
+        # Se status for None, assumimos que o banco vai usar o default "PENDENTE"
+        self.status = status if status is not None else "Pendente"
+
+    def __repr__(self):
+        return f"<Tarefa {self.id if self.id else 'Sem ID'} - {self.titulo}>"
+    
